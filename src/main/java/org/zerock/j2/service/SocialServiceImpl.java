c3487587c693228e44a197b3cc27436c9b546cf1
soapcandy <octi97@naver.com>
2023-07-18 12:49:50 +0900

    Access Token 가져오기

--- src/main/java/org/zerock/j2/service/SocialServiceImpl.java
+++ src/main/java/org/zerock/j2/service/SocialServiceImpl.java
@@ -2,11 +2,22 @@ package org.zerock.j2.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.LinkedHashMap;
 
 @Service
 @Log4j2
-public class SocialServiceImpl implements SocialService{
+public class SocialServiceImpl implements SocialService {
+
     @Value("${org.zerock.kakao.token_url}")
     private String tokenURL;
     @Value("${org.zerock.kakao.rest_key}")
@@ -14,17 +25,53 @@ public class SocialServiceImpl implements SocialService{
     @Value("${org.zerock.kakao.redirect_uri}")
     private String redirectURI;
     @Value("${org.zerock.kakao.get_user}")
-    private String  getUser;
+    private String getUser;
+
     @Override
     public String getKakaoEmail(String authCode) {
 
-        log.info("================================");
+        log.info("=================================");
         log.info("authCode: " + authCode);
         log.info("tokenURL: " + tokenURL);
         log.info("clientId: " + clientId);
         log.info("redirectURI: " + redirectURI);
         log.info("getUser: " + getUser);
 
+        String accessToken = getAccessToken(authCode);
+
         return null;
     }
+
+    private String getAccessToken(String authCode) {
+
+        String accessToken = null;
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(tokenURL)
+                .queryParam("grant_type", "authorization_code")
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", redirectURI)
+                .queryParam("code", authCode)
+                .build(true);
+
+        ResponseEntity<LinkedHashMap> response =
+                restTemplate.exchange(
+                        uriComponents.toString(), HttpMethod.POST, entity, LinkedHashMap.class);
+
+        log.info("-----------------------------------");
+        log.info(response);
+
+        LinkedHashMap<String, String> bodyMap = response.getBody();
+
+        accessToken = bodyMap.get("access_token");
+
+        log.info("Access Token: " + accessToken);
+
+        return accessToken;
+    }
+
 }
